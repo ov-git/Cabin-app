f2c6b54d42342384b596fc2b9fec0af87353240d
ov <virtaoskari@gmail.com>
2026-04-11 12:06:43 +0300

    placeholder functionality for cabins endpoint

---
+++ backend/Cabin-api/src/main/java/com/uef/cabinapi/controller/CabinController.java
@@ -0,0 +1,63 @@
+package com.uef.cabinapi.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+@RequestMapping("api/cabin")
+public class CabinController {
+    // private repository.CabinRepository cabinrepository;
+
+    // public CabinController(CabinRepository cabinrepository) {
+    // this.cabinrepository = cabinrepository;
+    // }
+    @GetMapping
+    public String findAll() throws IOException {
+        // return list of all cabins
+        InputStream input = new ClassPathResource("data/CabinsMock.json").getInputStream();
+        String json = new String(input.readAllBytes());
+
+        return json;
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Integer id) throws IOException {
+        // return cabin with given id
+        return "Cabin with id: " + id + " is not found";
+
+        // Optional<Cabin> = cabinrepository.findById(id);
+        // return cabin with given id
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String postCabin(@RequestBody String entity) {
+        //store the cabin in database and return the stored entity        
+        return entity;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCabin(@PathVariable Integer id) {
+        // delete the cabin with the given id
+        return "Cabin with id: " + id + " has been deleted";
+    }
+
+    @PutMapping("/{id}")
+    public String putCabin(@PathVariable Integer id, @RequestBody String entity) {
+        // update the cabin with the given id
+        return "Cabin with id: " + id + " has been updated";
+    }    
+
+}
