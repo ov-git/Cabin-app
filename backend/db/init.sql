4203253cd0f9a87c9ec67d19cfd33e908d870309
ov <virtaoskari@gmail.com>
2026-04-16 09:18:13 +0300

    localdev and docker environments + swagger plugin

---
+++ backend/db/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS cabin (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    price INT
+);
+
+INSERT INTO cabin (name, price) VALUES
+('Lake Cabin', 120),
+('Forest Cabin', 90);
