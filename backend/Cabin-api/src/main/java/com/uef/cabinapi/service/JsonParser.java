b6bc7b22756d8f2892b36556a20d4d2f31231237
valttkor <valttkor@uef.student.fi>
2026-04-23 23:30:38 +0300

    JsonParser-luokka pistetty aluilleen.

---
+++ backend/Cabin-api/src/main/java/com/uef/cabinapi/service/JsonParser.java
@@ -0,0 +1,24 @@
+/*
+Jäsennä JSON-tiedostoja.
+Sisältää mm. metodit parseUser, parseCabin jne., jotka muuttavat json-datan vastaavaksi luokaksi.
+ */
+
+package com.uef.cabinapi.service;
+
+public class JsonParser {
+    static void main() {
+
+    }
+    void parseUser() {
+        /*
+        Jäsentää parametrinä osoitetun JSON-tiedoston sisällön ja palauttaa User-olion.
+        TODO: vaihda paluuarvo User-tyyppiseksi, sitten kun User-luokka on määritelty
+         */
+    }
+    void parseCabin() {
+        /*
+        Jäsentää parametrinä osoitetun JSON-tiedoston sisällön ja palauttaa Cabin-olion.
+        TODO: vaihda paluuarvo Cabin-tyyppiseksi, sitten kun Cabin-luokka on määritelty
+         */
+    }
+}
